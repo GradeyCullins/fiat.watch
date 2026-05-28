95acb1ed2ea3582344db1d7eaeef8f5c15e965d7
Gradey Cullins <gradeycullins@gmail.com>
2026-05-27 22:54:50 -0600

    fix da credentials yuh

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << "mbp:3000"
+  config.hosts << "nas:3030"
 
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
